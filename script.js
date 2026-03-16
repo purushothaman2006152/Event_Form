856f334476cc6bfa723562029f50a264007997a2
purushothaman2006152 <purushothamane@srmasc.ac.in>
2026-03-16 13:47:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-function validateForm(){
+document.getElementById("registrationForm")
+.addEventListener("submit", function(e){
 
-let check=document.getElementById("confirm");
+e.preventDefault();
 
-if(!check.checked){
+let name=document.getElementById("name").value;
+let email=document.getElementById("email").value;
+let phone=document.getElementById("phone").value;
+let event=document.getElementById("event").value;
 
-alert("Please confirm that the details are correct.");
+let mode=document.querySelector('input[name="mode"]:checked').value;
 
-return false;
+let table=document.getElementById("participantsTable");
 
-}
+let newRow=document.createElement("tr");
 
-alert("Registration submitted successfully!");
+newRow.innerHTML=
+"<td>"+name+"</td>"+
+"<td>"+email+"</td>"+
+"<td>"+phone+"</td>"+
+"<td>"+event+"</td>"+
+"<td>"+mode+"</td>";
 
-return true;
+table.appendChild(newRow);
 
-}
+alert("Registration Successful!");
+
+document.getElementById("registrationForm").reset();
+
+});
